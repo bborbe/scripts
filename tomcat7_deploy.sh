8e594ca1b8d059f2fe8210c9660c8258502f1511
Benjamin Borbe <bborbe@rocketnews.de>
2012-12-20 15:54:17 +0100

    fix

--- tomcat7_deploy.sh
+++ tomcat7_deploy.sh
@@ -4,7 +4,7 @@ FILE=/home/bborbe/bridge.war
 
 if [ -f $FILE ]; then
         /etc/init.d/tomcat7 stop
-#       rm -rf /var/lib/tomcat7/webapps/bb* /tmp/tomcat7-temp /tmp/tomcat7-tomcat7-tmp /tmp/felix-storage /tmp/felix-cache
+        rm -rf /var/lib/tomcat7/webapps/bb* /tmp/tomcat7-temp /tmp/tomcat7-tomcat7-tmp /tmp/felix-storage /tmp/felix-cache
         mv $FILE /var/lib/tomcat7/webapps/bb.war
         /etc/init.d/tomcat7 start
 fi
