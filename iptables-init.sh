d809c9b7750109a4d418a1fc39375a91b001f2b8
Benjamin Borbe <bborbe@rocketnews.de>
2013-02-03 17:12:44 +0100

    add ssh port 1022

--- iptables-init.sh
+++ iptables-init.sh
@@ -102,6 +102,9 @@ $IPTABLES -A INPUT -i tap0 -p icmp --icmp-type 11 -j ACCEPT
 $IPTABLES -A INPUT -i eth0 -m state --state NEW --protocol tcp --dport 22 -j ACCEPT
 $IPTABLES -A INPUT -i eth1 -m state --state NEW --protocol tcp --dport 22 -j ACCEPT
 $IPTABLES -A INPUT -i tap0 -m state --state NEW --protocol tcp --dport 22 -j ACCEPT
+$IPTABLES -A INPUT -i eth0 -m state --state NEW --protocol tcp --dport 1022 -j ACCEPT
+$IPTABLES -A INPUT -i eth1 -m state --state NEW --protocol tcp --dport 1022 -j ACCEPT
+$IPTABLES -A INPUT -i tap0 -m state --state NEW --protocol tcp --dport 1022 -j ACCEPT
 # OpenVPN 
 $IPTABLES -A INPUT -i eth0 -m state --state NEW --protocol tcp -d 10.4.0.20 --dport 563 -j ACCEPT
 $IPTABLES -A INPUT -i eth1 -m state --state NEW --protocol tcp -d 10.4.0.20 --dport 563 -j ACCEPT
