6abb1a9308dabd65811d7ae24472c73ba6b47499
Benjamin Borbe <bborbe@rocketnews.de>
2013-04-14 22:05:58 +0200

    delete old dns entry

--- dns-update-home.benjamin-borbe.de.sh
+++ dns-update-home.benjamin-borbe.de.sh
@@ -2,12 +2,6 @@
 SERVER='ns.rocketnews.de'
 TTL='86400'
 KEY='/root/keys/Kserver1-ddns-key.+157+36654'
-cmd=$1
-zone=$2
-node=$3
-data=$4
-
-cmd=add 
 zone=benjamin-borbe.de 
 node=home
 data=`curl http://ip.benjamin-borbe.de`
@@ -15,7 +9,8 @@ class='A'
 tmpfile=$(mktemp)
 cat >$tmpfile <<END
 server $SERVER
-update $cmd ${node}.${zone} $TTL $class $data
+update delete ${node}.${zone} $TTL $class
+update add ${node}.${zone} $TTL $class $data
 send
 END
 nsupdate -k $KEY -v $tmpfile
--- dns-update.sh
+++ dns-update.sh
@@ -2,7 +2,7 @@
 SERVER='ns.rocketnews.de'
 TTL='86400'
 KEY='/root/keys/Kserver1-ddns-key.+157+36654'
-cmd=add 
+cmd=add
 zone=$1
 node=$2
 data=$3
@@ -10,7 +10,8 @@ class='A'
 tmpfile=$(mktemp)
 cat >$tmpfile <<END
 server $SERVER
-update $cmd ${node}.${zone} $TTL $class $data
+update delete ${node}.${zone} $TTL $class
+update add ${node}.${zone} $TTL $class $data
 send
 END
 nsupdate -k $KEY -v $tmpfile
