93c997804b072ef8ccf62e4ca4e961249ec0b221
Benjamin Borbe <bborbe@rocketnews.de>
2013-04-14 22:07:30 +0200

    silent

--- dns-update-home.benjamin-borbe.de.sh
+++ dns-update-home.benjamin-borbe.de.sh
@@ -4,7 +4,7 @@ TTL='86400'
 KEY='/root/keys/Kserver1-ddns-key.+157+36654'
 zone=benjamin-borbe.de 
 node=home
-data=`curl http://ip.benjamin-borbe.de`
+data=`curl -s http://ip.benjamin-borbe.de`
 class='A'
 tmpfile=$(mktemp)
 cat >$tmpfile <<END
