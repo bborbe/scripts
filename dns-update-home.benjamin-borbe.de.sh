d52e88266ab407cc4437c6c26a37fc921b98bf78
Benjamin Borbe <bborbe@rocketnews.de>
2013-04-14 21:58:51 +0200

    make static

--- dns-update-home.benjamin-borbe.de.sh
+++ dns-update-home.benjamin-borbe.de.sh
@@ -6,6 +6,11 @@ cmd=$1
 zone=$2
 node=$3
 data=$4
+
+cmd=add 
+zone=benjamin-borbe.de 
+node=home
+data=`curl http://ip.benjamin-borbe.de`
 class='A'
 tmpfile=$(mktemp)
 cat >$tmpfile <<END
@@ -15,7 +20,3 @@ send
 END
 nsupdate -k $KEY -v $tmpfile
 rm -f $tmpfile
-record=${node}.${zone}
-[[ ${zone} = *in-addr.arpa ]] && record=$(echo ${node}.${zone}  | awk -F"." '{ print $4"."$3"."$2"."$1} ' )
-echo -e "\n ==> Checking result: ${record}. ..."
-host $record $SERVER
