32b4f28fef90277e7f298d903902c6d9424ed06c
Benjamin Borbe <bborbe@rocketnews.de>
2013-08-21 10:21:33 +0200

    set permission

--- apt-cacher-clear.sh
+++ apt-cacher-clear.sh
@@ -4,5 +4,6 @@
 mv /var/cache/apt-cacher /var/cache/apt-cacher.old
 rm -rf /var/cache/apt-cacher.old &
 mkdir -p /var/cache/apt-cacher/{headers,import,packages,private,temp}
+chmod 0755 /var/cache/apt-cacher/*
 chown -R www-data:www-data /var/cache/apt-cacher
 /etc/init.d/apt-cacher start
