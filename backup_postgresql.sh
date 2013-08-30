368b51b3cf324cbbc9d974d918156a4ae59f03ba
Benjamin Borbe <bborbe@rocketnews.de>
2013-08-30 20:43:43 +0200

    add postgres backup script

---
+++ backup_postgresql.sh
@@ -0,0 +1,31 @@
+#!/bish/sh
+
+# add in /etc/crontab
+# 4 15 * * * root sh /root/scripts/backup_postgresql.sh >/dev/null 2>&1
+
+LOCKFILE=/var/run/backup_postgresql.pid
+BACKUP_NAME=/var/backups/postgres_`date '+%Y%m%d'`.sql.gz
+
+#
+# script below
+#
+
+echo "backup started"
+
+# lock script
+if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+  echo "already running"
+  exit
+fi
+
+# make sure the lockfile is removed when we exit and then claim it
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
+
+su postgres -c pg_dumpall | gzip -9 > $BACKUP_NAME
+
+# remove lock
+echo "remove lock"
+rm -f ${LOCKFILE}
+
+echo "backup finished"  
