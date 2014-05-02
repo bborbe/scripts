d9d5defeab664d96e39e5ed9c560cae4bcacbff8
Benjamin Borbe <bborbe@rocketnews.de>
2014-05-02 14:19:32 +0200

    extend sun iptables

--- sun-iptables-init.sh
+++ sun-iptables-init.sh
@@ -20,6 +20,37 @@ $IPTABLES -P FORWARD ACCEPT
 $IPTABLES -P OUTPUT ACCEPT
 
 #
+# Allow localhost
+#
+$IPTABLES -A INPUT -i lo -j ACCEPT
+$IPTABLES -A OUTPUT -o lo -j ACCEPT
+
+#
+# Ausgehende immer erlauben
+#
+$IPTABLES -A OUTPUT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+
+#
+# Antworten erlauben
+#
+$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+#
+# Ports freigeben
+#
+# Ping
+$IPTABLES -A INPUT -p icmp --icmp-type 8 -j ACCEPT
+$IPTABLES -A INPUT -p icmp --icmp-type 11 -j ACCEPT
+# SSH
+$IPTABLES -A INPUT -m state --state NEW --protocol tcp --dport 22 -j ACCEPT
+$IPTABLES -A INPUT -j ACCEPT
+
+#
+# Forward
+#
+$IPTABLES -A FORWARD -j ACCEPT
+
+#
 # Rest loggen
 #
 $IPTABLES -A INPUT -j LOG --log-prefix="IPTABLES-INPUT: "
