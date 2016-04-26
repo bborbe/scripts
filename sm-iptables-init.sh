a0db7e38f98be8e5e7f26d4c70593de5dcd43bd7
Benjamin Borbe <bborbe@rocketnews.de>
2016-04-26 19:43:29 +0200

    remove eth1 and tap0

--- sm-iptables-init.sh
+++ sm-iptables-init.sh
@@ -34,26 +34,18 @@ $IPTABLES -A OUTPUT -o lo -j ACCEPT
 # Ausgehende immer erlauben
 #
 $IPTABLES -A OUTPUT -o eth0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-$IPTABLES -A OUTPUT -o eth1 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-$IPTABLES -A OUTPUT -o tap0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
 
 #
 # Antworten erlauben
 #
 $IPTABLES -A INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-$IPTABLES -A INPUT -i eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
-$IPTABLES -A INPUT -i tap0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #
 # Ports freigeben
 #
 # Ping
 $IPTABLES -A INPUT -i eth0 -p icmp --icmp-type 8 -j ACCEPT
-$IPTABLES -A INPUT -i eth1 -p icmp --icmp-type 8 -j ACCEPT
-$IPTABLES -A INPUT -i tap0 -p icmp --icmp-type 8 -j ACCEPT
 $IPTABLES -A INPUT -i eth0 -p icmp --icmp-type 11 -j ACCEPT
-$IPTABLES -A INPUT -i eth1 -p icmp --icmp-type 11 -j ACCEPT
-$IPTABLES -A INPUT -i tap0 -p icmp --icmp-type 11 -j ACCEPT
 # SSH
 $IPTABLES -A INPUT -m state --state NEW --protocol tcp --dport 22 -j ACCEPT
 
