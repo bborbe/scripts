30cfc736065283798130c1030453a7dea5ce6b15
Benjamin Borbe <bborbe@rocketnews.de>
2016-05-16 11:09:07 +0200

    remove private

--- sm-iptables-init.sh
+++ sm-iptables-init.sh
@@ -53,20 +53,16 @@ $IPTABLES -A INPUT -m state --state NEW --protocol tcp --dport 22 -j ACCEPT
 # Portforwarding
 #
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 138.201.37.217 --dport 80 -j DNAT --to-destination 172.16.10.2:80
-$IPTABLES -t nat -A PREROUTING -i privatebr0 -p tcp -d 138.201.37.217 --dport 80 -j DNAT --to-destination 172.16.10.2:80
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 138.201.37.217 --dport 443 -j DNAT --to-destination 172.16.10.2:443
-$IPTABLES -t nat -A PREROUTING -i privatebr0 -p tcp -d 138.201.37.217 --dport 443 -j DNAT --to-destination 172.16.10.2:443
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 138.201.37.217 --dport 563 -j DNAT --to-destination 172.16.10.2:563
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 138.201.37.217 --dport 20001 -j DNAT --to-destination 172.16.10.2:20001
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 138.201.37.217 --dport 64738 -j DNAT --to-destination 172.16.10.2:64738
-
 
 #
 # Forward
 #
 $IPTABLES -A FORWARD -i eth0 -o privatebr0 -j ACCEPT
 $IPTABLES -A FORWARD -i privatebr0 -o eth0 -j ACCEPT
-$IPTABLES -A FORWARD -i privatebr0 -o privatebr0 -j ACCEPT
 
 #
 # Ports explizit sperren
