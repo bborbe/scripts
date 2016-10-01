3a168a7f79239c9a4ed22e83062a85aa94409a01
Benjamin Borbe <bborbe@rocketnews.de>
2016-10-01 14:37:15 +0200

    az24

---
+++ az24_md.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+ticket=$1
+echo "ticket ${ticket}"
+
+mkdir -p /Users/bborbe/Documents/Seibert-Media/Projects/Allianz/${ticket}
+filename="/Users/bborbe/Documents/Seibert-Media/Projects/Allianz/${ticket}/${ticket}.md"
+
+if [ ! -f "${filename}" ]; then
+	echo "create ${filename}"
+	cat /Users/bborbe/Documents/Seibert-Media/Projects/Allianz/template.md | sed -e "s,TICKET,${ticket},g;" > ${filename}
+fi
+
+echo "open ${filename}"
+open ${filename}
+
+cd $AZ24_HOME/allsecur-application/
+git checkout -b feature/${ticket} && git push --set-upstream origin feature/${ticket} || true
+git checkout feature/${ticket}
