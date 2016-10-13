2727f7d2e340c6f088cc537da87c57012d5a4ec6
Benjamin Borbe <bborbe@rocketnews.de>
2016-10-13 23:15:42 +0200

    remove 444

--- iptables-host1-om-init.sh
+++ iptables-host1-om-init.sh
@@ -59,7 +59,6 @@ $IPTABLES -A INPUT -m state --state NEW --protocol tcp --dport 22 -j ACCEPT
 #
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 163.172.222.137 --dport 80 -j DNAT --to-destination 172.16.60.2:1080
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 163.172.222.137 --dport 443 -j DNAT --to-destination 172.16.60.2:1443
-$IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 163.172.222.137 --dport 444 -j DNAT --to-destination 172.16.60.3:444
 $IPTABLES -t nat -A PREROUTING -i eth0 -p tcp -d 163.172.222.137 --dport 563 -j DNAT --to-destination 172.16.60.3:563
 
 #
