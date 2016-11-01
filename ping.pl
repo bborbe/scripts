cd2b6a883f36264af79079a6c99c2dd241a3a1ab
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-01 17:25:17 +0100

    update ip url

--- ping.pl
+++ ping.pl
@@ -9,8 +9,8 @@ $dt->set_time_zone( 'Europe/Berlin' );
 
 print $dt->datetime.' '.name()."\n";
 
-sub name {	
-	my $content = get('https://www.benjamin-borbe.de/ip/');
+sub name {
+  my $content = get('https://ip.benjamin-borbe.de');
 	if ($content eq '217.19.181.138') {
 		return 'seibert-media';
 	} else {
