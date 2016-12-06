107d1b7c37460264c16fba418afd55eb9ce26b57
Benjamin Borbe <bborbe@rocketnews.de>
2016-12-06 22:26:37 +0100

    change log args

--- cron_monitoring.sh
+++ cron_monitoring.sh
@@ -7,27 +7,30 @@ set -o pipefail
 export PATH=/opt/phantomjs/bin:$PATH
 
 /Users/bborbe/Documents/workspaces/go/bin/monitoring_cron \
--loglevel=INFO \
+-logtostderr \
+-v=2 \
 -concurrent 1 \
 -smtp-host=iredmail.mailfolder.org \
 -smtp-port=465 \
 -smtp-password=wYog90aOAdUBQinz \
 -smtp-tls=true \
 -smtp-tls-skip-verify=true \
--config=/Users/bborbe/Documents/workspaces/monitoring/local/config.xml \
+-config=/Users/bborbe/Documents/workspaces/kubernetes/monitoring/local/config.xml \
 >> ~/Library/Logs/monitoring.log 2>&1
 
 exit 0
 
 /Users/bborbe/Documents/workspaces/go/bin/monitoring_cron \
--loglevel=INFO \
+-logtostderr \
+-v=2 \
 -concurrent 1 \
 -smtp-password=wYog90aOAdUBQinz \
 -config=/Users/bborbe/Documents/workspaces/monitoring_configuration/playground.xml \
 >> ~/Library/Logs/monitoring.log 2>&1
 
 /Users/bborbe/Documents/workspaces/go/bin/monitoring_cron \
--loglevel=INFO \
+-logtostderr \
+-v=2 \
 -concurrent 1 \
 -smtp-password=wYog90aOAdUBQinz \
 -config=/Users/bborbe/Documents/workspaces/monitoring_configuration/codeyard-template.xml \
