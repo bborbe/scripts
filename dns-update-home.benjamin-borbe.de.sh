5508d459eb251d7ea28cda9ef662412951f99623
Benjamin Borbe <bborbe@rocketnews.de>
2016-12-08 10:28:34 +0100

    update ip url

--- dns-update-home.benjamin-borbe.de.sh
+++ dns-update-home.benjamin-borbe.de.sh
@@ -10,7 +10,7 @@ TTL='60'
 KEY='/root/keys/Kserver1-ddns-key.+157+36654'
 zone=benjamin-borbe.de 
 node=home
-data=`curl -s https://ip.tools.seibert-media.net`
+data=`curl -s https://ip.benjamin-borbe.de`
 class='A'
 tmpfile=$(mktemp)
 cat >$tmpfile <<END
--- dns-update-mobile-bb.benjamin-borbe.de.sh
+++ dns-update-mobile-bb.benjamin-borbe.de.sh
@@ -10,7 +10,7 @@ TTL='60'
 KEY="$HOME/.dns/Kserver1-ddns-key.+157+36654"
 zone=benjamin-borbe.de
 node=mobile-bb
-data=`curl -s https://ip.tools.seibert-media.net`
+data=`curl -s https://ip.benjamin-borbe.de`
 class='A'
 tmpfile=$(mktemp)
 cat >$tmpfile <<END
