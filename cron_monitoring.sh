a79897e33b78b1b50f4bbcddea64fdd18ce1aba3
Benjamin Borbe <bborbe@rocketnews.de>
2016-12-13 22:34:05 +0100

    remove unused

--- cron_monitoring.sh
+++ cron_monitoring.sh
@@ -10,29 +10,8 @@ export PATH=/opt/phantomjs/bin:$PATH
 -logtostderr \
 -v=2 \
 -concurrent 1 \
--smtp-host=iredmail.mailfolder.org \
--smtp-port=465 \
--smtp-password=wYog90aOAdUBQinz \
--smtp-tls=true \
--smtp-tls-skip-verify=true \
+-smtp-host=mail.benjamin-borbe.de \
+-smtp-port=25 \
 -config=/Users/bborbe/Documents/workspaces/kubernetes/monitoring/local/config.xml \
->> ~/Library/Logs/monitoring.log 2>&1
-
-exit 0
-
-/Users/bborbe/Documents/workspaces/go/bin/monitoring_cron \
--logtostderr \
--v=2 \
--concurrent 1 \
--smtp-password=wYog90aOAdUBQinz \
--config=/Users/bborbe/Documents/workspaces/monitoring_configuration/playground.xml \
->> ~/Library/Logs/monitoring.log 2>&1
-
-/Users/bborbe/Documents/workspaces/go/bin/monitoring_cron \
--logtostderr \
--v=2 \
--concurrent 1 \
--smtp-password=wYog90aOAdUBQinz \
--config=/Users/bborbe/Documents/workspaces/monitoring_configuration/codeyard-template.xml \
 >> ~/Library/Logs/monitoring.log 2>&1
 
