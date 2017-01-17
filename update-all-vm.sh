f30d427ead7150a63ced473cd06eedf569b18956
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-17 12:55:39 +0100

    add nuke

--- update-all-vm.sh
+++ update-all-vm.sh
@@ -4,6 +4,7 @@
 pssh -o /tmp/pssh.log -l bborbe -t 300 -p 100 \
 	-H rasp.hm.benjamin-borbe.de \
 	-H fire.hm.benjamin-borbe.de \
+	-H nuke.hm.benjamin-borbe.de \
 	-H sun.pn.benjamin-borbe.de \
 	-H tools.seibert-media.net \
 	"sudo dpkg --configure -a && sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get clean"
