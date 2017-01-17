63c6b856bad5af76c919e259965c9e2d549c7229
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-17 12:55:17 +0100

    update apps

--- app.sh
+++ app.sh
@@ -7,27 +7,27 @@ set -o pipefail
 case "$1" in
 	start)
 		echo "starting"
+		open -a 1Password\ 6 -g
 		open -a Alfred -g
-		open -a Viscosity -g
-		open -a FastScripts -g
 		open -a Caffeine -g
-		open /Users/bborbe/Library/PreferencePanes/HyperDock.prefpane/Contents/Resources/HyperDock\ Helper.app -g
-		open -a Google\ Chrome -g
-		open -a Mail -g
-		open -a iTunes -g
-		open -a NetNewsWire -g
 		open -a Calendar -g
+		open -a Emacs -g
+		open -a FastScripts -g
+		#open -a Firefox -g
+		open -a Google\ Chrome -g
 		open -a iTerm -g
+		open -a iTunes -g
+		open -a Mail -g
+		open -a NetNewsWire -g
+		open -a OmniFocus -g
+		open -a Salute -g
+		open -a Telegram -g
 		open -a Terminal -g
 		open -a TextEdit -g
-		open -a 1Password\ 6
-		open -a OmniFocus -g
-		open -a Telegram -g
+		open -a Thunderbird -g
 		open -a Twitter -g
-#		open -a Mattermost -g
-		open -a Emacs -g
-		open -a Firefox -g
-#		open -a Skype -g 
+		open -a Viscosity -g
+		open /Library/PreferencePanes/HyperDock.prefpane/Contents/Resources/HyperDock\ Helper.app -g		
 	;;
 	stop)
 		echo "stopping"
