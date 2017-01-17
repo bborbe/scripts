4ef71aafbb7a50630bba097bb0f25961e1d6c9fa
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-17 12:55:29 +0100

    update docker helpers

--- docker-delete-all.sh
+++ docker-delete-all.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-docker ps -a -q | xargs --no-run-if-empty docker rm -v
-docker images -q | xargs --no-run-if-empty docker rmi
+docker ps -a -q -f status=running | xargs --no-run-if-empty docker kill 
+docker ps -a -q -f status=exited | xargs --no-run-if-empty docker rm -v
+docker images -a -q | xargs --no-run-if-empty docker rmi -f
 
--- docker-kill-all.sh
+++ docker-kill-all.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-docker ps -a -q -f status=running | xargs --no-run-if-empty docker kill
+docker ps -a -q -f status=running | xargs --no-run-if-empty docker kill 
 
