2183b1eee6aa32c2f334906ed3577d059a8168b9
Benjamin Borbe <bborbe@rocketnews.de>
2017-07-01 12:52:58 +0200

    remove sm.tools

--- update-all-vm.sh
+++ update-all-vm.sh
@@ -6,5 +6,4 @@ pssh -o /tmp/pssh.log -l bborbe -t 300 -p 100 \
 	-H fire.hm.benjamin-borbe.de \
 	-H nuke.hm.benjamin-borbe.de \
 	-H sun.pn.benjamin-borbe.de \
-	-H tools.seibert-media.net \
 	"sudo dpkg --configure -a && sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get clean"
