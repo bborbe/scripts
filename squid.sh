18b857974adb44934f730289e2df2d333fca5a8b
Benjamin Borbe <bborbe@rocketnews.de>
2017-12-19 11:36:53 +0100

    improve squid restart

--- squid.sh
+++ squid.sh
@@ -4,22 +4,32 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+
+start() {
+	echo "starting"
+	sudo chown -R squid:squid /opt/local/etc/squid /opt/local/var/squid /opt/local/var/run/squid
+	sudo launchctl load -w /Library/LaunchDaemons/org.macports.Squid.plist
+}
+
+stop() {
+	echo "stopping"
+	sudo launchctl unload -w /Library/LaunchDaemons/org.macports.Squid.plist || true
+	sleep 2
+	ps ax | grep '(squid-1) -s' | grep -v grep | awk '{ print $1 }' | sudo xargs --no-run-if-empty kill -9 || true
+	ps ax | grep 'squid -s'     | grep -v grep | awk '{ print $1 }' | sudo xargs --no-run-if-empty kill -9 || true
+}
+
 case "$1" in
 	start)
-		echo "starting"
-		sudo chown -R squid:squid /opt/local/etc/squid /opt/local/var/squid /opt/local/var/run/squid
-		sudo launchctl load -w /Library/LaunchDaemons/org.macports.Squid.plist
+		start
 	;;
 	stop)
-		echo "stopping"
-		sudo launchctl unload -w /Library/LaunchDaemons/org.macports.Squid.plist 
-		sleep 2
-		sudo kill -9 `ps ax | grep '(squid)' | grep -v grep | awk '{ print $1 }'`
+		stop
 	;;
 	restart)
-		$0 stop
+		stop
 		sleep 1
-		$0 start
+		start
 	;;
 	tail)
 		sudo tail -n 500 -f /opt/local/var/squid/logs/*.log
