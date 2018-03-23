788586ccce1755a2440ad34b4d88edee7f37dbe4
Benjamin Borbe <bborbe@rocketnews.de>
2018-03-23 11:27:52 +0100

    add clone docker image script

---
+++ docker_image_clone.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+SOURCE="$1"
+TARGET="$2"
+
+echo "clone ${SOURCE} to ${TARGET}"
+
+docker pull ${SOURCE} || true
+sleep 2
+docker tag `docker images ${SOURCE} -q` ${TARGET}
+gcloud docker --server=eu.gcr.io -- push ${TARGET}
