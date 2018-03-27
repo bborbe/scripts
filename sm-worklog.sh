ad02fb625d43e36cd4538c886d5c042aacead498
Benjamin Borbe <bborbe@rocketnews.de>
2018-03-27 13:16:06 +0200

    add python

--- sm-worklog.sh
+++ sm-worklog.sh
@@ -2,6 +2,6 @@
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-list=$(find ~/Documents/workspaces/sm-* $GO/src/bitbucket.apps.seibert-media.net $GO/src/github.com/bborbe $GO/src/bitbucket.org/bborbe -name .git -type d -prune -exec dirname {} \;)
+list=$(find ~/Documents/workspaces/sm-* ~/Documents/workspaces/python/* $GO/src/bitbucket.apps.seibert-media.net $GO/src/github.com/bborbe $GO/src/bitbucket.org/bborbe -name .git -type d -prune -exec dirname {} \;)
 
 worklog -v=0 -logtostderr -days 20 -author "Benjamin Borbe" -dir $(join_by , $list) | sort | uniq
