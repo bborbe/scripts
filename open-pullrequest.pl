0aa5c12dbe9e64a6a26f9fc7807ad8b1126a110a
Benjamin Borbe <bborbe@rocketnews.de>
2018-05-17 15:49:45 +0200

    allow https git repos

--- open-pullrequest.pl
+++ open-pullrequest.pl
@@ -6,11 +6,11 @@ use warnings;
 my $branch = `git rev-parse --abbrev-ref HEAD 2> /dev/null`;
 $branch =~ s/^\s+|\s+$//g;
 my $remote = `git remote get-url origin`;
-$remote =~ /git@([^:]+):.*?([^\/]+)\/([^\/]+)\.git$/;
+$remote =~ /(git@|https:\/\/)([^:]+)[:\/].*?([^\/]+)\/([^\/]+?)(\.git)?$/;
 my $url;
-if ($1 eq 'github.com') {
-    $url = "https://github.com/$2/$3/compare/${branch}?expand=1";
+if ($2 eq 'github.com') {
+    $url = "https://github.com/$3/$4/compare/${branch}?expand=1";
 } else {
-    $url = "https://$1/projects/$2/repos/$3/compare/commits?sourceBranch=refs/heads/$branch";
+    $url = "https://$2/projects/$3/repos/$4/compare/commits?sourceBranch=refs/heads/$branch";
 }
 exec ("open '$url'");
