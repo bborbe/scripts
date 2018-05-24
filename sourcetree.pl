8e172efa96c349861bdd135d7874036468e8be96
Benjamin Borbe <bborbe@rocketnews.de>
2018-05-24 13:14:16 +0200

    rewrite sourcetree

---
+++ sourcetree.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Cwd;
+use File::Basename;
+
+
+my $path;
+if (@ARGV > 0) {
+    $path = $ARGV[0];
+} else {
+    $path = Cwd::getcwd();
+}
+$path = Cwd::abs_path($path);
+
+while ( ! -d $path.'/.git' ) {
+    $path = dirname( $path ); 
+    if ( $path eq '/' ) {
+	print('cant find .git directory'."\n");
+	exit(1);
+    }
+}
+
+exec ("open -a SourceTree \"$path\"");
+
