383df3323f28bf0f7455c877f7d9ce6541e2c4f3
Benjamin Borbe <bborbe@rocketnews.de>
2018-06-06 12:04:36 +0200

    remove apps

--- app.sh
+++ app.sh
@@ -16,15 +16,11 @@ fi
 case "$1" in
 	start)
 		echo "starting"
-		#open -a 1Password\ 6 -g
 		open -a Alfred\ 3 -g
 		open -a Caffeine -g
 		open -a Calendar -g
-		#open -a Discord -g
-		#open -a Emacs -g
 		open -a Enpass -g
 		open -a FastScripts -g
-		#open -a Firefox -g
 		open -a Google\ Chrome -g
 		open -a iTerm -g
 		open -a iTunes -g
@@ -33,11 +29,7 @@ case "$1" in
 		open -a OmniFocus -g
 		open -a Salute -g
 		open -a Telegram -g
-		#open -a Wire -g
 		open -a Terminal -g
-		#open -a TextEdit -g
-		#open -a Thunderbird -g
-		#open -a Twitter -g
 		open -a Viscosity -g
 		open /Library/PreferencePanes/HyperDock.prefpane/Contents/Resources/HyperDock\ Helper.app -g		
 		open ~/Documents/notes.txt
