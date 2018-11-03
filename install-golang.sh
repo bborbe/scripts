a0f5c6fdfc0c531031b21a05ac6058c49d922dd0
Benjamin Borbe <bborbe@rocketnews.de>
2018-11-03 18:57:04 +0100

    fix replace link

--- install-golang.sh
+++ install-golang.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 set -o errtrace
 
-GO_VERSION=${GO_VERSION:-"1.10.1"}
+GO_VERSION=${GO_VERSION:-"1.11.2"}
 GO_TARGET="/opt/go${GO_VERSION}"
 GO_ROOT="/opt/go"
 
@@ -16,7 +16,8 @@ echo "install Go ${GO_VERSION} ${GO_ARCH} to ${GO_TARGET}. please enter password
 
 sudo mkdir -p "${GO_TARGET}"
 curl -Ls "https://dl.google.com/go/go${GO_VERSION}.${GO_ARCH}-amd64.tar.gz" | sudo tar -xz --directory "${GO_TARGET}" --strip-components=1 --no-same-owner 
-sudo ln --symbolic --force "${GO_TARGET}" "${GO_ROOT}"
+sudo sudo rm -f "${GO_ROOT}"
+sudo ln --symbolic "${GO_TARGET}" "${GO_ROOT}"
 
 echo 'add to ~/.bash_profile'
 echo 'GOPATH="$HOME/go"'
