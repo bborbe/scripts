84bc976b818f9dd470db3a818c4b959fb4c68c76
Benjamin Borbe <bborbe@rocketnews.de>
2019-04-30 14:19:55 +0200

    remove deprecated arg

--- k8s-delete-error.sh
+++ k8s-delete-error.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl get pods --all-namespaces -a | grep -E "CrashLoopBackOff|Evicted|OOMKilled|Error|ImagePullBackOff" | awk '{print $2 " -n " $1}' | xargs --no-run-if-empty kubectl delete pod
+kubectl get pods --all-namespaces | grep -E "CrashLoopBackOff|Evicted|OOMKilled|Error|ImagePullBackOff" | awk '{print $2 " -n " $1}' | xargs --no-run-if-empty kubectl delete pod
