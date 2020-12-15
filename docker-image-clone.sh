b4167d6e0d02fae0d2c6561e3a14725f6ce0fbbb
Benjamin Borbe <bborbe@rocketnews.de>
2020-12-15 11:29:07 +0100

    remove server

--- docker-image-clone.sh
+++ docker-image-clone.sh
@@ -10,7 +10,7 @@ TARGET="$2"
 
 echo "clone ${SOURCE} to ${TARGET}"
 
-gcloud docker --server=eu.gcr.io -- pull ${SOURCE} || true
+docker pull ${SOURCE} || true
 sleep 2
 docker tag `docker images ${SOURCE} -q` ${TARGET}
-gcloud docker --server=eu.gcr.io -- push ${TARGET}
+docker push ${TARGET}
