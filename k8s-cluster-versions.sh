2fdf56832730c122d6dffee4427d3e5b3f6ac415
Benjamin Borbe <bborbe@rocketnews.de>
2020-12-15 11:28:42 +0100

    add k8s cluster

--- k8s-cluster-versions.sh
+++ k8s-cluster-versions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for cluster in fire nuke sun netcup; do
+for cluster in hetzner-1 fire nuke sun netcup; do
 	kubectl --context ${cluster} version|grep 'Server Version'|awk "{print \"${cluster}: \" \$5}"
 done 
