2edddd847020b5fb155a166b117a077dfcf57941
Benjamin Borbe <bborbe@rocketnews.de>
2021-10-25 15:10:28 +0200

    add go mod require script

---
+++ replace-go-mod-require.pl
@@ -0,0 +1,19 @@
+#!perl
+
+use strict;
+
+my $filename = 'go.mod';
+
+open(FH, '<', $filename) or die $!;
+my $content = '';
+while(<FH>){
+   $content .= $_;
+}
+close(FH);
+
+$content =~ s/require.*\)//s;
+$content =~ s/\n+/\n/g;
+
+open(FH, '>', $filename) or die $!;
+print FH $content;
+close(FH);
