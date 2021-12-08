5b3cd8921e6e9bc3162620312ff6af1124b7b2cf
Benjamin Borbe <bborbe@rocketnews.de>
2021-12-08 11:31:45 +0100

    remove netcup server

--- update-all.sh
+++ update-all.sh
@@ -13,7 +13,6 @@ pssh -o /tmp/pssh.log -l bborbe -t 300 -p 100 \
 	-H fire.hm.benjamin-borbe.de \
 	-H hell.hm.benjamin-borbe.de \
 	-H sun.pn.benjamin-borbe.de \
-	-H v22016124049440903.goodsrv.de \
 	-H hetzner-1.benjamin-borbe.de \
 	"sudo dpkg --configure -a && sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get clean"
 
