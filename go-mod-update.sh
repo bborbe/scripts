af857497393f7dcd4a10fad34d7d9b52302b2d3d
Benjamin Borbe <bborbe@rocketnews.de>
2022-10-18 12:50:36 +0200

    fix go mod update

--- go-mod-update.sh
+++ go-mod-update.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-go get -u $(go list -mod=mod -f 'if not (or .Main .Indirect).Pathend' -m all)
+go get -u $(go list -mod=mod -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)
 go mod tidy
 go mod vendor
