dc1c7056e36706bd9a5e71b4a64285e9f996613e
Benjamin Borbe <bborbe@rocketnews.de>
2023-05-11 09:19:29 +0200

    remove git repos

--- sm-worklog.sh
+++ sm-worklog.sh
@@ -2,6 +2,6 @@
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-list=$(find ~/Documents/workspaces/gartentechnik/gartentechnik ~/Documents/workspaces/kubernetes/manifests ~/Documents/workspaces/sm-* ~/Documents/workspaces/python/* $GO/src/bitbucket.apps.seibert-media.net $GO/src/github.com/bborbe $GO/src/github.com/seibert-media $GO/src/bitbucket.org/bborbe -name .git -type d -prune -exec dirname {} \;)
+list=$(find ~/Documents/workspaces/sm-* ~/Documents/workspaces/python/* $GO/src/bitbucket.apps.seibert-media.net $GO/src/github.com/bborbe $GO/src/github.com/seibert-media $GO/src/bitbucket.org/bborbe -name .git -type d -prune -exec dirname {} \;)
 
 worklog -v=0 -logtostderr -days 20 -author "Benjamin Borbe" -dir $(join_by , $list) | sort | uniq
