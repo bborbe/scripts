05f2a3b394942454318e5eac78343bab2cab85ed
Benjamin Borbe <bborbe@rocketnews.de>
2023-10-18 23:19:14 +0200

    improve go mod update

--- go-mod-update.sh
+++ go-mod-update.sh
@@ -4,6 +4,21 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-go get -u $(go list -mod=mod -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)
+# old version
+# go get -u $(go list -mod=mod -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)
+
+while true
+do
+    notupdated=true;
+    for i in $(go list -mod=mod -m -u all | grep ']$'| awk '{print $1}'); do
+        go get -u ${i};
+        notupdated=false;
+    done
+    if [ $notupdated ]; then
+        echo "update completed";
+        break;
+    fi
+done
+
 go mod tidy
 go mod vendor
