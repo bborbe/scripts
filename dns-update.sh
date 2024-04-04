98a6f7781c043c3b8ea38269669187e1276f1cf4
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-04-04 15:59:58 +0200

    improve dns-update script

--- dns-update.sh
+++ dns-update.sh
@@ -4,20 +4,24 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SERVER='ns.rocketnews.de'
-TTL='86400'
-KEY='/root/keys/Kserver1-ddns-key.+157+36654'
-cmd=add
-zone=$1
-node=$2
-data=$3
+server="$1"
+key="$2"
+zone="$3"
+node="$4"
+ip="$5"
+
+echo "update ${node}.${zone} with $ip started"
+
+ttl='60'
 class='A'
 tmpfile=$(mktemp)
 cat >$tmpfile <<END
-server $SERVER
-update delete ${node}.${zone} $TTL $class
-update add ${node}.${zone} $TTL $class $data
+server $server
+update delete ${node}.${zone} $ttl $class
+update add ${node}.${zone} $ttl $class $ip
 send
 END
-nsupdate -k $KEY -v $tmpfile
+nsupdate -k $key -v $tmpfile
 rm -f $tmpfile
+
+echo "update ${node}.${zone} with $ip finished"
