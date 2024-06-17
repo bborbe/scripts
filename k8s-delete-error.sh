e76dc409f90382818f9be1c6b8b454a07a377e79
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-06-17 14:01:27 +0200

    improve k8s delete error

--- k8s-delete-error.sh
+++ k8s-delete-error.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-kubectl get pods --all-namespaces | grep -E "NotReady|Unknown|InvalidImageName|CrashLoopBackOff|Evicted|OOMKilled|Error|ImagePullBackOff" | awk '{print $2 " -n " $1}' | xargs --no-run-if-empty kubectl delete pod
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+for ns in $(kubectl get pods --all-namespaces | grep -E "NotReady|Unknown|InvalidImageName|CrashLoopBackOff|Evicted|OOMKilled|Error|ImagePullBackOff" | awk '{print $1}' | sort | uniq); do
+echo "delete pods in namespace $ns";
+kubectl -n "$ns" get pods | grep -E "NotReady|Unknown|InvalidImageName|CrashLoopBackOff|Evicted|OOMKilled|Error|ImagePullBackOff" | awk '{print $1}' | xargs --no-run-if-empty kubectl -n "$ns" delete pod;
+done
