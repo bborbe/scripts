29ad8214bb2959355bdc09e1d42a8ba1154251df
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-06-22 10:34:50 +0200

    add k3s shutdown script

---
+++ k3s-shutdown.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+systemctl stop k3s
+systemctl stop k3s-agent
+/usr/local/bin/k3s-killall.sh
+mkdir -p /var/lib/rancher/k3s/storage
+mount /var/lib/rancher/k3s/storage
