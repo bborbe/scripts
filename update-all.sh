7bcea1a240bf48d715a4d2e5fb9b2689ba58677b
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-10-13 14:15:50 +0200

    add vms

--- update-all.sh
+++ update-all.sh
@@ -8,15 +8,20 @@ set -o pipefail
 /opt/local/bin/pssh -o /tmp/pssh.log -l bborbe -t 300 -p 100 \
 	-H co2hz.hm.benjamin-borbe.de \
 	-H co2wz.hm.benjamin-borbe.de \
-	-H rasp3.hm.benjamin-borbe.de \
-	-H rasp4.hm.benjamin-borbe.de \
-	-H fire.hm.benjamin-borbe.de \
-	-H nuke.hm.benjamin-borbe.de \
-	-H hell.hm.benjamin-borbe.de \
-	-H hetzner-1.benjamin-borbe.de \
+	-H fire-k3s-dev-capitalcom.hm.benjamin-borbe.de \
+	-H fire-k3s-dev-mt5.hm.benjamin-borbe.de \
 	-H fire-k3s-dev.hm.benjamin-borbe.de \
 	-H fire-k3s-master.hm.benjamin-borbe.de \
+	-H fire-k3s-prod-capitalcom.hm.benjamin-borbe.de \
+	-H fire-k3s-prod-mt5.hm.benjamin-borbe.de \
 	-H fire-k3s-prod.hm.benjamin-borbe.de \
+	-H fire.hm.benjamin-borbe.de \
+	-H hell.hm.benjamin-borbe.de \
+	-H hetzner-1.benjamin-borbe.de \
+	-H nuke-k3s-agent.hm.benjamin-borbe.de \
+	-H nuke.hm.benjamin-borbe.de \
+	-H rasp3.hm.benjamin-borbe.de \
+	-H rasp4.hm.benjamin-borbe.de \
 	"sudo dpkg --configure -a && sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get clean"
 
 echo "run port selfupdate"
