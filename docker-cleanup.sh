4221e05914292ffee0c233be01867d96047138a7
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-10-13 20:29:18 +0200

    improve cleanup

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
 docker ps -a -q -f status=exited | xargs --no-run-if-empty docker rm -v
 docker images -f "dangling=true" -q | xargs --no-run-if-empty docker rmi
+docker system prune -af
+docker volume prune -f
