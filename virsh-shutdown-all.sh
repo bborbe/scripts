a4fbdf253ae0a5c00da21249e798a589c52aae70
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-10-13 14:16:18 +0200

    add virsh shutdown all

---
+++ virsh-shutdown-all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+virsh list --all | grep 'running' | awk '{print $2}' | xargs --no-run-if-empty virsh shutdown
