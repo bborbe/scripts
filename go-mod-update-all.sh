d8405012782a225fec4f88c72f60f7993878b0b3
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-01-09 11:41:03 +0100

    add go mod update all

---
+++ go-mod-update-all.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# old version
+# go get -u $(go list -mod=mod -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)
+
+while true
+do
+    notupdated=true;
+    #for i in $(go list -mod=mod -m -u all | grep ']$'| awk '{print $1}'); do
+    for i in $(go list -mod=mod -m all | awk '{print $1}'); do
+        go get -u ${i};
+        notupdated=false;
+    done
+    if [ $notupdated ]; then
+        echo "update completed";
+        break;
+    fi
+done
+
+go mod tidy
+go mod vendor
