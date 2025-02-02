4e519e273f7e274f08ac06d7442eea6529cec52d
Benjamin Borbe <bborbe@rocketnews.de>
2025-02-02 09:19:57 +0000

    add longhorn-3

--- remote-k3s-shutdown-nuke.sh
+++ remote-k3s-shutdown-nuke.sh
@@ -12,6 +12,7 @@ parallel-ssh -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
 	-H nuke-k3s-longhorn-0.hm.benjamin-borbe.de \
 	-H nuke-k3s-longhorn-1.hm.benjamin-borbe.de \
 	-H nuke-k3s-longhorn-2.hm.benjamin-borbe.de \
+	-H nuke-k3s-longhorn-3.hm.benjamin-borbe.de \
 	-H nuke-k3s-master-0.hm.benjamin-borbe.de \
 	-H nuke-k3s-master-1.hm.benjamin-borbe.de \
 	-H nuke-k3s-master-2.hm.benjamin-borbe.de \
