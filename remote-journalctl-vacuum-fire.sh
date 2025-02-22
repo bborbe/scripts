03802f97ab126b073c256f2886475b6ad2a01ea3
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-02-22 23:58:36 +0100

    add vacuum

---
+++ remote-journalctl-vacuum-fire.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# -P
+parallel-ssh -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
+	-H fire.hm.benjamin-borbe.de \
+	"sudo journalctl --vacuum-size=1M"
+
+
+echo "journalctl vacuum-size triggered"
