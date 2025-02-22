5c5faf4e492566e8462d30902477bdf1883c38ed
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-02-22 23:49:38 +0100

    rm

--- sm-worklog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function join_by { local IFS="$1"; shift; echo "$*"; }
-
-list=$(find ~/Documents/workspaces/sm-* ~/Documents/workspaces/python/* $GO/src/bitbucket.apps.seibert-media.net $GO/src/github.com/bborbe $GO/src/github.com/seibert-media $GO/src/bitbucket.org/bborbe -name .git -type d -prune -exec dirname {} \;)
-
-worklog -v=0 -logtostderr -days 20 -author "Benjamin Borbe" -dir $(join_by , $list) | sort | uniq
