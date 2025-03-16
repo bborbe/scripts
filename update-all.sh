152edf9886ccb14903643a3722cd91b14d4d7672
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-03-16 16:12:01 +0100

    remove rasp4

--- update-all.sh
+++ update-all.sh
@@ -18,8 +18,7 @@ $CMD -o /tmp/pssh.log -l bborbe -t 300 -p 100 \
 	-H hetzner-1.benjamin-borbe.de \
 	-H nuke.hm.benjamin-borbe.de \
 	-H rasp3.hm.benjamin-borbe.de \
-	-H rasp4.hm.benjamin-borbe.de \
-  -H nuke-k3s-agent-0.hm.benjamin-borbe.de \
+	-H nuke-k3s-agent-0.hm.benjamin-borbe.de \
   -H nuke-k3s-agent-1.hm.benjamin-borbe.de \
   -H nuke-k3s-dev-capitalcom.hm.benjamin-borbe.de \
   -H nuke-k3s-dev-core.hm.benjamin-borbe.de \
