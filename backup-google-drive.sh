ecc8de12799bae0615bb3230e55cb4016d81db47
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-04-06 14:04:19 +0200

    add borbe familie

--- backup-google-drive.sh
+++ backup-google-drive.sh
@@ -7,4 +7,5 @@ set -o pipefail
 rsync -av --progress \
 /Users/bborbe/My\ Drive\ \(benjamin.borbe@gmail.com\) \
 /Users/bborbe/My\ Drive\ \(borbe.capital@gmail.com\) \
+/Users/bborbe/My\ Drive\ \(borbefamilie@gmail.com\)/ \
 bborbe@hell.hm.benjamin-borbe.de:/home/bborbe/Backups/Google\ Drive/
