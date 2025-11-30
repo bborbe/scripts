7aa076eca563b7dacfba863012c369e1bf5fa608
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-11-30 10:13:40 +0100

    split k3s shutdown into separate host groups
    Run shutdown commands in batches: agents/dev/prod/longhorn first,
    then kafka nodes, then masters last.

--- remote-k3s-shutdown-nuke.sh
+++ remote-k3s-shutdown-nuke.sh
@@ -14,16 +14,21 @@ $CMD -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
   -H nuke-k3s-agent-0.hm.benjamin-borbe.de \
   -H nuke-k3s-agent-1.hm.benjamin-borbe.de \
   -H nuke-k3s-dev-0.hm.benjamin-borbe.de \
+  -H nuke-k3s-prod-0.hm.benjamin-borbe.de \
   -H nuke-k3s-longhorn-0.hm.benjamin-borbe.de \
   -H nuke-k3s-longhorn-1.hm.benjamin-borbe.de \
-  -H nuke-k3s-prod-0.hm.benjamin-borbe.de \
+	"sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
+
+$CMD -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
   -H nuke-k3s-kafka-0.hm.benjamin-borbe.de \
   -H nuke-k3s-kafka-1.hm.benjamin-borbe.de \
   -H nuke-k3s-kafka-2.hm.benjamin-borbe.de \
+	"sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
+
+$CMD -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
   -H nuke-k3s-master-0.hm.benjamin-borbe.de \
   -H nuke-k3s-master-1.hm.benjamin-borbe.de \
   -H nuke-k3s-master-2.hm.benjamin-borbe.de \
 	"sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
-
 
 echo "shutdown k3s triggered"
