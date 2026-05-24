85465f46764af6b6d68242654e7078295443b99a
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-05-24 12:49:43 +0200

    release v0.1.0

--- pi.sh
+++ pi.sh
@@ -1,4 +1,8 @@
-#!bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 BRAVE_SEARCH_API_KEY="$(teamvault-password --teamvault-config ~/.teamvault.json --teamvault-key dwkkzw)" \
 MINIMAX_API_KEY="$(teamvault-password --teamvault-config ~/.teamvault.json --teamvault-key MOPmQL)" \
