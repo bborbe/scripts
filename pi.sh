f412295671b3807b8b49590d274240a44aafb375
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-05-26 22:56:30 +0200

    improve pi.sh

--- pi.sh
+++ pi.sh
@@ -6,4 +6,7 @@ set -o pipefail
 
 BRAVE_SEARCH_API_KEY="$(teamvault-password --teamvault-config ~/.teamvault.json --teamvault-key dwkkzw)" \
 MINIMAX_API_KEY="$(teamvault-password --teamvault-config ~/.teamvault.json --teamvault-key MOPmQL)" \
-pi --provider minimax --model MiniMax-M2.7-highspeed
+pi \
+--provider minimax \
+--model MiniMax-M2.7-highspeed \
+"$@"
