7fa1e049eca570d46014031d31631069f9d2977e
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-07-20 11:49:23 +0200

    add new vm

--- remote-k3s-shutdown-nuke.sh
+++ remote-k3s-shutdown-nuke.sh
@@ -14,23 +14,22 @@ $CMD -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
   -H nuke-k3s-agent-0.hm.benjamin-borbe.de \
   -H nuke-k3s-dev-0.hm.benjamin-borbe.de \
   -H nuke-k3s-prod-0.hm.benjamin-borbe.de \
-	"sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
+  -H nuke-k3s-prod-worker-0.hm.benjamin-borbe.de \
+  -H nuke-k3s-dev-worker-0.hm.benjamin-borbe.de \
+  "sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
 
 $CMD -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
   -H nuke-k3s-kafka-0.hm.benjamin-borbe.de \
   -H nuke-k3s-kafka-1.hm.benjamin-borbe.de \
   -H nuke-k3s-kafka-2.hm.benjamin-borbe.de \
-	"sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
+  "sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
 
 $CMD -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
   -H nuke-k3s-master-0.hm.benjamin-borbe.de \
   -H nuke-k3s-master-1.hm.benjamin-borbe.de \
   -H nuke-k3s-master-2.hm.benjamin-borbe.de \
-	"sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
-
-$CMD -o /tmp/pssh-k3s.log -l bborbe -t 300 -p 100 \
-  -H nuke-k3s-prod.hm.benjamin-borbe.de \
-  -H nuke-k3s-dev.hm.benjamin-borbe.de \
-	"sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
+  -H nuke-k3s-prod-master-0.hm.benjamin-borbe.de \
+  -H nuke-k3s-dev-master-0.hm.benjamin-borbe.de \
+  "sudo systemctl stop k3s;sudo systemctl stop k3s-agent;sudo /usr/local/bin/k3s-killall.sh;sudo mkdir -p /var/lib/rancher/k3s/storage;sudo mount /var/lib/rancher/k3s/storage;echo done"
 
 echo "shutdown k3s triggered"
